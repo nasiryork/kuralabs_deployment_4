17597b9336c5b7ae85e46efafd18a331104efb2f
Nasir York <nasiryork@gmail.com>
2022-10-28 20:48:07 -0400

    Added VPC Module

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -2,16 +2,36 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 provider "aws" {
+  region = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-1"
-  
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "d4vpc"
+  cidr = "172.40.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  #private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["172.40.0.0/18", "172.40.64.0/18"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+
+  }
 }
 
 resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   key_name = "master_key"
+  subnet_id = "${element(module.vpc.public_subnets, 0)}"
   vpc_security_group_ids = [aws_security_group.web_ssh.id]
 
   user_data = "${file("deploy.sh")}"
